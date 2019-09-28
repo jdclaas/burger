c3f18c70c0c6005b07f81e13128147adfb77a411
jdclaas <jd_claas@icloud.com>
2019-09-27 20:26:10 -0500

    final

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Eggers Can't Be Cheesers served with fried egg & cheese', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cheeses Is Born Burger comes served with baby Swiss cheese', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Open Sesame Burger served open-faced on a sesame seed bun', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Blondes Have More Fun-gus Burger comes with mushrooms', FALSE);
+
+TRUNCATE TABLE burgers;
